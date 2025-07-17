b1c365dffde52d33b0108b32e0ad0aa7842430d2
Steve Dodd <steved424@gmail.com>
2025-07-17 20:57:49 +0100

    Split off gnucash flake

--- flake.nix
+++ flake.nix
@@ -3,43 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+#    gnucashfl.url = "/vol/home-ssd/steved-ssd-data/src/nixos/gnucash54";
+    gnucashfl.url = "github:srd424/gnucash54-nix";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, gnucashfl }: let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+#      gnucash = gnucashfl.packages.x86_64-linux.gnucash;
+      pymodule = gnucashfl.packages.x86_64-linux.pymodule;
     in {
-    packages.x86_64-linux.gnucash =
-      nixpkgs.legacyPackages.x86_64-linux.gnucash.overrideAttrs (prevAttrs: {
-        patches = (prevAttrs.patches or []) ++ [
-          ./python-env.patch
-         ];
-        cmakeFlags = [
-          "-DWITH_PYTHON=\"ON\""
-          "-DPYTHON_SYSCONFIG_BUILD=\"$out\""
-        ];
-        buildInputs = prevAttrs.buildInputs ++ (with pkgs; [
-          python3
-        ]);
-        postPatch = ''
-          substituteInPlace bindings/python/__init__.py \
-          --subst-var-by gnc_dbd_dir "${pkgs.libdbiDrivers}/lib/dbd" \
-          --subst-var-by gsettings_schema_dir ${pkgs.glib.makeSchemaPath "$out" "gnucash-${prevAttrs.version}"};
-        '';
-      });
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.gnucash;
-    packages.x86_64-linux.pymodule = pkgs.python3Packages.toPythonModule self.packages.x86_64-linux.gnucash;
     packages.x86_64-linux.import = pkgs.python3Packages.buildPythonApplication rec {
       pname = "gnucash-qif-import";
       version = "0.1";
       src = /home/steved/pkgs/gnucash-qif-import/gnucash-qif-import3;
       format = "other";
-      propagatedBuildInputs = [ self.outputs.packages.x86_64-linux.pymodule ];
+      propagatedBuildInputs = [ pymodule ];
       installPhase = ''
         mkdir -p $out/bin
         cp qif.py import.py $out/bin
       '';
     };
+    packages.x86_64-linux.default = self.packages.x86_64-linux.import;
 
   };
 }
